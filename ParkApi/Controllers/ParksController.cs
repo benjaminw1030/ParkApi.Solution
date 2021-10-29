e581d72c76fa45df58be0d0e860047ec6c4140c6
Ben Wilson <benjaminw1030@gmail.com>
2021-10-29 14:02:35 -0700

    add swagger data tags to controller

--- ParkApi/Controllers/ParksController.cs
+++ ParkApi/Controllers/ParksController.cs
@@ -25,17 +25,16 @@ namespace ParkApi.AddControllers
     /// <remarks>
     /// Sample request:
     ///
-    ///     GET /Group
-    ///     {
-    ///        "id": 1,
-    ///        "name": "Group1",
-    ///        "messages": []
-    ///     }
+    ///     GET /api/parks
     ///
     /// </remarks>
     /// <param name="name"></param>
-    /// <returns>A specific list of groups with that name.</returns>
-    /// <response code="201">Returns the groups</response>
+    /// <returns>A specific list of parks with part or all of that name.</returns>
+    /// <param name="category"></param>
+    /// <returns>A specific list of parks with part or all of that category.</returns>
+    /// <param name="state"></param>
+    /// <returns>A specific list of parks with part or all of that state.</returns>
+    /// <response code="201">Returns valid parks</response>
     /// <response code="400">If the item is null</response>   
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Park>>> Get(string name, string category, string state)
@@ -56,7 +55,27 @@ namespace ParkApi.AddControllers
       return await query.ToListAsync();
     }
 
-    // POST api/parks
+    /// <summary>
+    /// Posts a new park.
+    /// </summary>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     POST /api/parks
+    /// {
+    ///     "name": "Silver Falls",
+    ///     "category": "State",
+    ///     "state": "Oregon",
+    ///     "longitude": -122.65,
+    ///     "latitude": 44.85,
+    ///     "area": 36,
+    ///     "visitors": 1100000,
+    ///     "estDate": "1933-07-23T00:00:00"
+    /// }
+    ///
+    /// </remarks>
+    /// <response code="201">Returns created park</response>
+    /// <response code="400">If there is a validation error</response>   
     [HttpPost]
     public async Task<ActionResult<Park>> Post(Park park)
     {
@@ -65,7 +84,16 @@ namespace ParkApi.AddControllers
       return CreatedAtAction(nameof(GetPark), new { id = park.ParkId }, park);
     }
 
-    // GET api/parks/5
+    /// <summary>
+    /// Gets a specific park at an ID.
+    /// </summary>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     GET /api/parks/1
+    ///
+    /// </remarks>
+    /// <response code="404">If park not found</response>   
     [HttpGet("{id}")]
     public async Task<ActionResult<Park>> GetPark(int id)
     {
@@ -77,7 +105,28 @@ namespace ParkApi.AddControllers
       return park;
     }
 
-    // PUT: api/parks/5
+    /// <summary>
+    /// Edits a specific park at an ID.
+    /// </summary>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///    PUT /api/parks/1
+    /// {
+    ///     "parkId": 1
+    ///     "name": "Silver Falls",
+    ///     "category": "State",
+    ///     "state": "Oregon",
+    ///     "longitude": -122.65,
+    ///     "latitude": 44.85,
+    ///     "area": 36,
+    ///     "visitors": 1100000,
+    ///     "estDate": "1933-07-23T00:00:00"
+    /// }
+    ///
+    /// </remarks>
+    /// <response code="400">If there is a validation error</response>   
+    /// <response code="404">If park not found</response>   
     [HttpPut("{id}")]
     public async Task<ActionResult<Park>> Put(int id, Park park)
     {
@@ -104,7 +153,28 @@ namespace ParkApi.AddControllers
       return NoContent();
     }
 
-    // PUT: api/parks/5
+    /// <summary>
+    /// Edits a specific park at an ID (via PATCH).
+    /// </summary>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///    PATCH /api/parks/1
+    /// {
+    ///     "parkId": 1
+    ///     "name": "Silver Falls",
+    ///     "category": "State",
+    ///     "state": "Oregon",
+    ///     "longitude": -122.65,
+    ///     "latitude": 44.85,
+    ///     "area": 36,
+    ///     "visitors": 1100000,
+    ///     "estDate": "1933-07-23T00:00:00"
+    /// }
+    ///
+    /// </remarks>
+    /// <response code="400">If there is a validation error</response>   
+    /// <response code="404">If park not found</response>   
     [HttpPatch("{id}")]
     public async Task<ActionResult<Park>> Patch(int id, Park park)
     {
@@ -131,7 +201,17 @@ namespace ParkApi.AddControllers
       return NoContent();
     }
 
-    // DELETE: api/parks/5
+    /// <summary>
+    /// Deletes a specific park at an ID.
+    /// </summary>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///    DELETE /api/parks/1
+    ///
+    /// </remarks>
+    /// <response code="204">If park is deleted</response>   
+    /// <response code="404">If park not found</response> 
     [HttpDelete("{id}")]
     public async Task<ActionResult> DeletePark(int id)
     {
@@ -145,7 +225,6 @@ namespace ParkApi.AddControllers
       return NoContent();
     }
 
-    // PATCH: api/parks/5
     private bool ParkExists(int id)
     {
       return _db.Parks.Any(e => e.ParkId == id);
