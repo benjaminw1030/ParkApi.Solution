04d371cdb78c26615c266cb8103159d4b8d5772c
Ben Wilson <benjaminw1030@gmail.com>
2021-10-29 10:53:53 -0700

    Create migration, endpoint testing

---
+++ ParkApi/Migrations/20211029170730_Initial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace parkapi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parks",
+                columns: table => new
+                {
+                    ParkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Category = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    State = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Longitude = table.Column<double>(type: "double", nullable: false),
+                    Latitude = table.Column<double>(type: "double", nullable: false),
+                    Area = table.Column<double>(type: "double", nullable: false),
+                    Visitors = table.Column<int>(type: "int", nullable: false),
+                    EstDate = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parks", x => x.ParkId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Area", "Category", "EstDate", "Latitude", "Longitude", "Name", "State", "Visitors" },
+                values: new object[] { 1, 4926.0799999999999, "National", new DateTime(1919, 2, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), 36.060000000000002, -112.14, "Grand Canyon", "Arizona", 2897098 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parks");
+        }
+    }
+}
--- ParkApi/Models/Park.cs
+++ ParkApi/Models/Park.cs
@@ -12,10 +12,10 @@ namespace ParkApi.Models
     public string Category { get; set; }
     [Required (ErrorMessage = "A state is required.")]
     public string State { get; set; }
-    [Range (0, 180,
+    [Range (-180, 180,
       ErrorMessage = "Value for Longitude must be between -180 and 180")]
     public double Longitude { get; set; }
-    [Range(0, 90,
+    [Range(-90, 90,
       ErrorMessage = "Value for Latitude must be between -90 and 90")]
     public double Latitude { get; set; }
     public double Area { get; set; }
